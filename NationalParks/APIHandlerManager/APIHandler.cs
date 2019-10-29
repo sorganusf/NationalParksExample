71c295155f3ef43d02ed7518aefbd48f3d2921c2
baliusf <kartikaybali@mail.usf.edu>
2019-10-29 15:45:22 -0400

    Final commit

---
+++ NationalParks/APIHandlerManager/APIHandler.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using NationalParks.Models;
+using Newtonsoft.Json;
+
+namespace NationalParks.APIHandlerManager
+{
+    public class APIHandler
+    {
+        static string BASE_URL = "https://developer.nps.gov/api/v1/";
+        static string API_KEY = ""; //Add your API key here inside ""
+
+        HttpClient httpClient;
+
+        public APIHandler()
+        {
+            httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Accept.Clear();
+            httpClient.DefaultRequestHeaders.Add("X-Api-Key", API_KEY);
+            httpClient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public Parks GetParks()
+        {
+            string NATIONAL_PARK_API_PATH = BASE_URL + "/parks?limit=20";
+            string parksData = "";
+
+            Parks parks = null;
+
+            httpClient.BaseAddress = new Uri(NATIONAL_PARK_API_PATH);
+            HttpResponseMessage response = httpClient.GetAsync(NATIONAL_PARK_API_PATH).GetAwaiter().GetResult();
+            if (response.IsSuccessStatusCode)
+            {
+                parksData = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+            }
+
+            if (!parksData.Equals(""))
+            {
+                parks = JsonConvert.DeserializeObject<Parks>(parksData);
+            }
+            return parks;
+        }
+    }
+}
---
+++ NationalParks/Models/Parks.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NationalParks.Models
+{
+    public class Park
+    {
+        public string states { get; set; }
+        public string latLong { get; set; }
+        public string description { get; set; }
+        public string designation { get; set; }
+        public string parkCode { get; set; }
+        public string id { get; set; }
+        public string directionsInfo { get; set; }
+        public string directionsUrl { get; set; }
+        public string fullName { get; set; }
+        public string url { get; set; }
+        public string weatherInfo { get; set; }
+        public string name { get; set; }
+    }
+
+    public class Parks
+    {
+        public string total { get; set; }
+        public List<Park> data { get; set; }
+        public string limit { get; set; }
+        public string start { get; set; }
+    }
+}
+
