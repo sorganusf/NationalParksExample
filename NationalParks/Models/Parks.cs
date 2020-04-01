44dc9c3122ce5a2a01218ec44125662ad0465747
Sean O <54554261+sorganusf@users.noreply.github.com>
2020-03-31 22:20:12 -0400

    Update Parks.cs

--- NationalParks/Models/Parks.cs
+++ NationalParks/Models/Parks.cs
@@ -8,25 +8,15 @@ namespace NationalParks.Models
   // or sites such as JsonToCSHarp
   public class Park
   {
-    public string states { get; set; }
-    public string latLong { get; set; }
-    public string description { get; set; }
-    public string designation { get; set; }
-    public string parkCode { get; set; }
-    public string id { get; set; }
-    public string directionsInfo { get; set; }
-    public string directionsUrl { get; set; }
-    public string fullName { get; set; }
-    public string url { get; set; }
-    public string weatherInfo { get; set; }
-    public string name { get; set; }
+    public string official_title { get; set; }
+    public string page_count { get; set; }
+    public string byte_size { get; set; }
+
   }
 
   public class Parks
   {
-    public string total { get; set; }
-    public List<Park> data { get; set; }
-    public string limit { get; set; }
-    public string start { get; set; }
+    public string location { get; set; }
+    public List<Park> results { get; set; }
   }
 }
